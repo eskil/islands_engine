d7e73233760a9feaa48263da615bb8c298010179
Eskil Heyn Olsen <eskil@eskil.org>
2021-07-31 00:00:37 +0200

    Add Island.forested check and test

--- test/island_test.exs
+++ test/island_test.exs
@@ -5,7 +5,7 @@ defmodule IslandsEngine.IslandTest do
   doctest IslandsEngine.Island, import: true
 
   test "island guess a hit" do
-    {:ok, island} = Island.new(:dot, Coordinate.new!(1, 1))
+    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
     {:ok, coordinate} = Coordinate.new(1, 1)
 
     # Verify a hit and the return value
@@ -14,10 +14,24 @@ defmodule IslandsEngine.IslandTest do
   end
 
   test "island guess a miss" do
-    {:ok, island} = Island.new(:dot, Coordinate.new!(1, 1))
-    {:ok, coordinate} = Coordinate.new(2, 1)
+    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+    {:ok, coordinate} = Coordinate.new(8, 8)
 
     # Verify a miss
     :miss = Island.guess(island, coordinate)
+  end
+
+  test "island forested" do
+    {:ok, island} = Island.new(:dot, Coordinate.new!(1, 1))
+    {:ok, coordinate} = Coordinate.new(1, 1)
+    {:hit, hit_island} = Island.guess(island, coordinate)
+    assert Island.forested?(hit_island) == true
+  end
+
+  test "island not forested" do
+    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+    {:ok, coordinate} = Coordinate.new(1, 1)
+    {:hit, hit_island} = Island.guess(island, coordinate)
+    assert Island.forested?(hit_island) == false
   end
 end
