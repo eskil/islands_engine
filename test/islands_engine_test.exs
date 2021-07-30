e2e18d989a6f412b5493a3c83ccd980da682b409
Eskil Heyn Olsen <eskil@eskil.org>
2021-07-30 23:20:47 +0200

    Unit test for Guesses.add

--- test/islands_engine_test.exs
+++ test/islands_engine_test.exs
@@ -1,8 +1,38 @@
 defmodule IslandsEngineTest do
   use ExUnit.Case
+  alias IslandsEngine.{Coordinate, Guesses}
   doctest IslandsEngine
 
-  test "greets the world" do
-    assert IslandsEngine.hello() == :world
+  test "board add" do
+    guesses = Guesses.new()
+
+    # Add a hit
+    {:ok, hit1} = Coordinate.new(8, 3)
+    guesses = Guesses.add(guesses, :hit, hit1)
+    assert guesses.hits == MapSet.new([Coordinate.new!(8, 3)])
+    assert guesses.misses == MapSet.new([])
+
+    # Add a second hit
+    {:ok, hit2} = Coordinate.new(9, 7)
+    guesses = Guesses.add(guesses, :hit, hit2)
+    assert guesses.hits == MapSet.new([Coordinate.new!(8, 3), Coordinate.new!(9, 7)])
+    assert guesses.misses == MapSet.new([])
+
+    # Readd a  hit
+    guesses = Guesses.add(guesses, :hit, hit1)
+    assert guesses.hits == MapSet.new([Coordinate.new!(8, 3), Coordinate.new!(9, 7)])
+    assert guesses.misses == MapSet.new([])
+
+    # Add a miss
+    {:ok, miss1} = Coordinate.new(1, 2)
+    guesses = Guesses.add(guesses, :miss, miss1)
+    assert guesses.hits == MapSet.new([Coordinate.new!(8, 3), Coordinate.new!(9, 7)])
+    assert guesses.misses == MapSet.new([Coordinate.new!(1, 2)])
+
+    # Add a second miss
+    {:ok, miss2} = Coordinate.new(2, 2)
+    guesses = Guesses.add(guesses, :miss, miss2)
+    assert guesses.hits == MapSet.new([Coordinate.new!(8, 3), Coordinate.new!(9, 7)])
+    assert guesses.misses == MapSet.new([Coordinate.new!(1, 2), Coordinate.new!(2, 2)])
   end
 end
