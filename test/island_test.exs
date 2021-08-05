5764d4fb4d7a497586d0f778da036d31691645fd
Eskil Heyn Olsen <eskil@eskil.org>
2021-08-05 14:28:12 +0200

    Testing out exunit describe, fixtures and setup

--- test/island_test.exs
+++ test/island_test.exs
@@ -4,49 +4,58 @@ defmodule IslandsEngine.IslandTest do
   alias IslandsEngine.Coordinate
   doctest IslandsEngine.Island, import: true
 
-  test "new!" do
-    island = Island.new!(:dot, Coordinate.new!(10, 10))
-    assert island
+  describe "island" do
+    test "new" do
+      {:ok, island} = Island.new(:dot, Coordinate.new!(10, 10))
+      assert island
 
-    assert_raise ArgumentError, "invalid island type", fn ->
-      Island.new!(:ok, Coordinate.new!(10, 10))
+      {:error, :invalid_island_type} = Island.new(:error, Coordinate.new!(10, 10))
+      {:error, :invalid_coordinate} = Island.new(:square, Coordinate.new!(10, 10))
     end
 
-    assert_raise ArgumentError, "invalid coordinate", fn ->
-      Island.new!(:square, Coordinate.new!(10, 10))
+    test "new!" do
+      island = Island.new!(:dot, Coordinate.new!(10, 10))
+      assert island
+
+      assert_raise ArgumentError, "invalid island type", fn ->
+        Island.new!(:ok, Coordinate.new!(10, 10))
+      end
+
+      assert_raise ArgumentError, "invalid coordinate", fn ->
+        Island.new!(:square, Coordinate.new!(10, 10))
+      end
+    end
+
+    test "guess a hit" do
+      {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, coordinate} = Coordinate.new(1, 1)
+
+      # Verify a hit and the return value
+      {:hit, hit_island} = Island.guess(island, coordinate)
+      assert hit_island.hit_coordinates == MapSet.new([coordinate])
+    end
+
+    test "guess a miss" do
+      {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, coordinate} = Coordinate.new(8, 8)
+
+      # Verify a miss
+      :miss = Island.guess(island, coordinate)
+    end
+
+    test "guess a hit and is not forested" do
+      {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+      {:hit, island} = Island.guess(island, Coordinate.new!(1, 1))
+      assert Island.forested?(island) == false
+    end
+
+    test "guess all hits and is forested" do
+      {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
+      {:hit, island} = Island.guess(island, Coordinate.new!(1, 1))
+      {:hit, island} = Island.guess(island, Coordinate.new!(1, 2))
+      {:hit, island} = Island.guess(island, Coordinate.new!(2, 1))
+      {:hit, island} = Island.guess(island, Coordinate.new!(2, 2))
+      assert Island.forested?(island) == true
     end
   end
-
-  test "island guess a hit" do
-    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, coordinate} = Coordinate.new(1, 1)
-
-    # Verify a hit and the return value
-    {:hit, hit_island} = Island.guess(island, coordinate)
-    assert hit_island.hit_coordinates == MapSet.new([coordinate])
-  end
-
-  test "island guess a miss" do
-    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, coordinate} = Coordinate.new(8, 8)
-
-    # Verify a miss
-    :miss = Island.guess(island, coordinate)
-  end
-
-  test "island guess a hit and is not forested" do
-    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
-    {:hit, island} = Island.guess(island, Coordinate.new!(1, 1))
-    assert Island.forested?(island) == false
-  end
-
-  test "island guess all hits and is forested" do
-    {:ok, island} = Island.new(:square, Coordinate.new!(1, 1))
-    {:hit, island} = Island.guess(island, Coordinate.new!(1, 1))
-    {:hit, island} = Island.guess(island, Coordinate.new!(1, 2))
-    {:hit, island} = Island.guess(island, Coordinate.new!(2, 1))
-    {:hit, island} = Island.guess(island, Coordinate.new!(2, 2))
-    assert Island.forested?(island) == true
-  end
-
 end
--- test/rules_test.exs
+++ test/rules_test.exs
@@ -3,6 +3,16 @@ defmodule IslandsEngine.RulesTest do
   alias IslandsEngine.Rules
   doctest IslandsEngine.Rules, import: true
 
+  def fixture(:ready_player1) do
+    rules = Rules.new()
+    {:ok, rules} = Rules.check(rules, :add_player)
+    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
+    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+    {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+    {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
+    rules
+  end
+
   test "starts initialised" do
     rules = Rules.new()
     assert rules.state == :initialised
@@ -73,5 +83,15 @@ defmodule IslandsEngine.RulesTest do
     :error = Rules.check(rules, {:position_island, :player2})
 
     assert rules.state == :player1_turn
+  end
+
+  setup _ready_player1 do
+    rules = fixture(:ready_player1)
+    {:ok, [rules: rules]}
+  end
+
+  test "player 1 makes turn", ready_player1 do
+    rules = ready_player1.rules
+    IO.puts inspect(rules)
   end
 end
