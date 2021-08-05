6d3ca96e88651bae44548efeceef5b5383029ed8
Eskil Heyn Olsen <eskil@eskil.org>
2021-08-05 15:19:18 +0200

    More testing out exunit describe, fixtures and setup

--- test/board_test.exs
+++ test/board_test.exs
@@ -3,92 +3,94 @@ defmodule IslandsEngine.BoardTest do
   alias IslandsEngine.{Board, Island, Coordinate}
   doctest IslandsEngine.Board, import: true
 
-  test "position new island without overlap" do
-    # test add two islands without overlap
-    board = Board.new()
-    {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
-    board = Board.position_island(board, :square, square)
-    board = Board.position_island(board, :dot, dot)
-    assert Map.keys(board) == [:dot, :square]
-  end
+  describe "board" do
+    test "position new island without overlap" do
+      # test add two islands without overlap
+      board = Board.new()
+      {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
+      board = Board.position_island(board, :square, square)
+      board = Board.position_island(board, :dot, dot)
+      assert Map.keys(board) == [:dot, :square]
+    end
 
-  test "position new island with overlap" do
-    # test add two islands with overlap
-    board = Board.new()
-    {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(2, 2))
-    board = Board.position_island(board, :square, square)
-    {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
-  end
+    test "position new island with overlap" do
+      # test add two islands with overlap
+      board = Board.new()
+      {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(2, 2))
+      board = Board.position_island(board, :square, square)
+      {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
+    end
 
-  test "re-position island without overlap" do
-    # test add two islands without overlap
-    # then reposition first island without overlap
-    board = Board.new()
-    {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
-    board = Board.position_island(board, :square, square)
-    board = Board.position_island(board, :dot, dot)
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(6, 6))
-    board = Board.position_island(board, :dot, dot)
-    assert Map.keys(board) == [:dot, :square]
-  end
+    test "re-position island without overlap" do
+      # test add two islands without overlap
+      # then reposition first island without overlap
+      board = Board.new()
+      {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
+      board = Board.position_island(board, :square, square)
+      board = Board.position_island(board, :dot, dot)
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(6, 6))
+      board = Board.position_island(board, :dot, dot)
+      assert Map.keys(board) == [:dot, :square]
+    end
 
-  test "re-position island with overlap" do
-    # test add two islands without overlap
-    # then reposition first island with overlap
-    board = Board.new()
-    {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
-    board = Board.position_island(board, :square, square)
-    board = Board.position_island(board, :dot, dot)
-    {:ok, dot} = Island.new(:dot, Coordinate.new!(1, 1))
-    {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
-  end
+    test "re-position island with overlap" do
+      # test add two islands without overlap
+      # then reposition first island with overlap
+      board = Board.new()
+      {:ok, square} = Island.new(:square, Coordinate.new!(1, 1))
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(5, 5))
+      board = Board.position_island(board, :square, square)
+      board = Board.position_island(board, :dot, dot)
+      {:ok, dot} = Island.new(:dot, Coordinate.new!(1, 1))
+      {:error, :overlapping_island} = Board.position_island(board, :dot, dot)
+    end
 
-  test "all islands positioned" do
-    # position all islands, check that all_islands_positioned is false until the last
-    board = Board.new()
-    board = Board.position_island(board, :square, Island.new!(:square, Coordinate.new!(1, 1)))
-    assert Board.all_islands_positioned?(board) == false
-    board = Board.position_island(board, :atoll, Island.new!(:atoll, Coordinate.new!(1, 3)))
-    assert Board.all_islands_positioned?(board) == false
-    board = Board.position_island(board, :l_shape, Island.new!(:l_shape, Coordinate.new!(3, 1)))
-    assert Board.all_islands_positioned?(board) == false
-    board = Board.position_island(board, :s_shape, Island.new!(:s_shape, Coordinate.new!(1, 5)))
-    assert Board.all_islands_positioned?(board) == false
-    board = Board.position_island(board, :dot, Island.new!(:dot, Coordinate.new!(10, 10)))
-    assert Board.all_islands_positioned?(board) == true
-  end
+    test "all islands positioned" do
+      # position all islands, check that all_islands_positioned is false until the last
+      board = Board.new()
+      board = Board.position_island(board, :square, Island.new!(:square, Coordinate.new!(1, 1)))
+      assert Board.all_islands_positioned?(board) == false
+      board = Board.position_island(board, :atoll, Island.new!(:atoll, Coordinate.new!(1, 3)))
+      assert Board.all_islands_positioned?(board) == false
+      board = Board.position_island(board, :l_shape, Island.new!(:l_shape, Coordinate.new!(3, 1)))
+      assert Board.all_islands_positioned?(board) == false
+      board = Board.position_island(board, :s_shape, Island.new!(:s_shape, Coordinate.new!(1, 5)))
+      assert Board.all_islands_positioned?(board) == false
+      board = Board.position_island(board, :dot, Island.new!(:dot, Coordinate.new!(10, 10)))
+      assert Board.all_islands_positioned?(board) == true
+    end
 
-  test "hit all islands" do
-    # position all islands in the upper part except :dot at 10, 10
-    # in two loops (Enum.each(Coordinate.board_range), Board.guess
-    # it should be false until the 10, 10th is hit
-    board = Board.new()
-    board = Board.position_island(board, :square, Island.new!(:square, Coordinate.new!(1, 1)))
-    board = Board.position_island(board, :atoll, Island.new!(:atoll, Coordinate.new!(1, 3)))
-    board = Board.position_island(board, :l_shape, Island.new!(:l_shape, Coordinate.new!(3, 1)))
-    board = Board.position_island(board, :s_shape, Island.new!(:s_shape, Coordinate.new!(1, 5)))
-    board = Board.position_island(board, :dot, Island.new!(:dot, Coordinate.new!(10, 10)))
-    assert Board.all_islands_positioned?(board) == true
+    test "hit all islands" do
+      # position all islands in the upper part except :dot at 10, 10
+      # in two loops (Enum.each(Coordinate.board_range), Board.guess
+      # it should be false until the 10, 10th is hit
+      board = Board.new()
+      board = Board.position_island(board, :square, Island.new!(:square, Coordinate.new!(1, 1)))
+      board = Board.position_island(board, :atoll, Island.new!(:atoll, Coordinate.new!(1, 3)))
+      board = Board.position_island(board, :l_shape, Island.new!(:l_shape, Coordinate.new!(3, 1)))
+      board = Board.position_island(board, :s_shape, Island.new!(:s_shape, Coordinate.new!(1, 5)))
+      board = Board.position_island(board, :dot, Island.new!(:dot, Coordinate.new!(10, 10)))
+      assert Board.all_islands_positioned?(board) == true
 
-    # 1. Check a hit on the first island, but not forested and no win
-    {:hit, :none, :no_win, board} = Board.guess(board, Coordinate.new!(1, 1))
+      # 1. Check a hit on the first island, but not forested and no win
+      {:hit, :none, :no_win, board} = Board.guess(board, Coordinate.new!(1, 1))
 
-    # Carpet forest the map, except the three cases we test separately
-    targets = for row <- Coordinate.board_range, col <- Coordinate.board_range, do: {row, col}
-    targets = Enum.reject(targets, fn {row, col} -> {row, col} in [{1, 1}, {10, 9}, {10, 10}] end)
-    board = Enum.reduce(targets, board, fn {row, col}, board ->
-      {_hit_or_miss, _forested, _win, board} = Board.guess(board, Coordinate.new!(row, col))
-      board
-    end)
+      # Carpet forest the map, except the three cases we test separately
+      targets = for row <- Coordinate.board_range, col <- Coordinate.board_range, do: {row, col}
+      targets = Enum.reject(targets, fn {row, col} -> {row, col} in [{1, 1}, {10, 9}, {10, 10}] end)
+      board = Enum.reduce(targets, board, fn {row, col}, board ->
+        {_hit_or_miss, _forested, _win, board} = Board.guess(board, Coordinate.new!(row, col))
+        board
+      end)
 
-    # 2. Check a miss on the last row
-    {:miss, :none, :no_win, board} = Board.guess(board, Coordinate.new!(10, 9))
+      # 2. Check a miss on the last row
+      {:miss, :none, :no_win, board} = Board.guess(board, Coordinate.new!(10, 9))
 
-    # 2. Check a hit and forest on the :dot, and win
-    {:hit, :dot, :win, _board} = Board.guess(board, Coordinate.new!(10, 10))
+      # 2. Check a hit and forest on the :dot, and win
+      {:hit, :dot, :win, _board} = Board.guess(board, Coordinate.new!(10, 10))
+    end
   end
 end
--- test/rules_test.exs
+++ test/rules_test.exs
@@ -13,85 +13,87 @@ defmodule IslandsEngine.RulesTest do
     rules
   end
 
-  test "starts initialised" do
-    rules = Rules.new()
-    assert rules.state == :initialised
-  end
-
-  test "add player moves to players set" do
-    rules = Rules.new()
-    assert rules.state == :initialised
-
-    {:ok, rules} = Rules.check(rules, :add_player)
-    assert rules.state == :players_set
-  end
-
-  test "players positions islands" do
-    rules = Rules.new()
-    {:ok, rules} = Rules.check(rules, :add_player)
-
-    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
-    assert rules.state == :players_set
-
-    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
-    assert rules.state == :players_set
-
-    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
-    assert rules.state == :players_set
-
-    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
-    assert rules.state == :players_set
-
-    assert_raise KeyError, fn ->
-      Rules.check(rules, {:position_island, :player3})
+  describe "rules" do
+    setup _ready_player1 do
+      rules = fixture(:ready_player1)
+      {:ok, [rules: rules]}
     end
-  end
 
-  test "players set islands" do
-    rules = Rules.new()
-    {:ok, rules} = Rules.check(rules, :add_player)
-    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
-    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+    test "starts initialised" do
+      rules = Rules.new()
+      assert rules.state == :initialised
+    end
 
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
-    assert rules.state == :players_set
-    assert rules.player1 == :islands_set
-    assert rules.player2 == :islands_not_set
+    test "add player moves to players set" do
+      rules = Rules.new()
+      assert rules.state == :initialised
 
-    # It's ok to set islands twice while state remains :players_set
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
-    assert rules.state == :players_set
-    assert rules.player1 == :islands_set
-    assert rules.player2 == :islands_not_set
+      {:ok, rules} = Rules.check(rules, :add_player)
+      assert rules.state == :players_set
+    end
 
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
-    assert rules.state == :player1_turn
-    assert rules.player1 == :islands_set
-    assert rules.player2 == :islands_set
-  end
+    test "players positions islands" do
+      rules = Rules.new()
+      {:ok, rules} = Rules.check(rules, :add_player)
 
-  test "players cannot position islands after set islands" do
-    rules = Rules.new()
-    {:ok, rules} = Rules.check(rules, :add_player)
-    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
-    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+      {:ok, rules} = Rules.check(rules, {:position_island, :player1})
+      assert rules.state == :players_set
 
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
-    :error = Rules.check(rules, {:position_island, :player1})
+      {:ok, rules} = Rules.check(rules, {:position_island, :player1})
+      assert rules.state == :players_set
 
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
-    :error = Rules.check(rules, {:position_island, :player2})
+      {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+      assert rules.state == :players_set
 
-    assert rules.state == :player1_turn
-  end
+      {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+      assert rules.state == :players_set
 
-  setup _ready_player1 do
-    rules = fixture(:ready_player1)
-    {:ok, [rules: rules]}
-  end
+      assert_raise KeyError, fn ->
+        Rules.check(rules, {:position_island, :player3})
+      end
+    end
 
-  test "player 1 makes turn", ready_player1 do
-    rules = ready_player1.rules
-    IO.puts inspect(rules)
+    test "players set islands" do
+      rules = Rules.new()
+      {:ok, rules} = Rules.check(rules, :add_player)
+      {:ok, rules} = Rules.check(rules, {:position_island, :player1})
+      {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      assert rules.state == :players_set
+      assert rules.player1 == :islands_set
+      assert rules.player2 == :islands_not_set
+
+      # It's ok to set islands twice while state remains :players_set
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      assert rules.state == :players_set
+      assert rules.player1 == :islands_set
+      assert rules.player2 == :islands_not_set
+
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
+      assert rules.state == :player1_turn
+      assert rules.player1 == :islands_set
+      assert rules.player2 == :islands_set
+    end
+
+    test "players cannot position islands after set islands" do
+      rules = Rules.new()
+      {:ok, rules} = Rules.check(rules, :add_player)
+      {:ok, rules} = Rules.check(rules, {:position_island, :player1})
+      {:ok, rules} = Rules.check(rules, {:position_island, :player2})
+
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
+      :error = Rules.check(rules, {:position_island, :player1})
+
+      {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
+      :error = Rules.check(rules, {:position_island, :player2})
+
+      assert rules.state == :player1_turn
+    end
+
+    test "player 1 makes turn", ready_player1 do
+      rules = ready_player1.rules
+      IO.puts inspect(rules)
+    end
   end
 end
