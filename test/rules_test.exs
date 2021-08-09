37d286fc3c31e4d0f94c883967fee719bbc04cba
Eskil Heyn Olsen <eskil@eskil.org>
2021-08-09 14:52:38 -0700

    Endgame rules and tests

--- test/rules_test.exs
+++ test/rules_test.exs
@@ -5,20 +5,22 @@ defmodule IslandsEngine.RulesTest do
 
   def fixture(:ready_player1) do
     rules = Rules.new()
-    {:ok, rules} = Rules.check(rules, :add_player)
-    {:ok, rules} = Rules.check(rules, {:position_island, :player1})
-    {:ok, rules} = Rules.check(rules, {:position_island, :player2})
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player1})
-    {:ok, rules} = Rules.check(rules, {:set_islands, :player2})
-    rules
+    %Rules{rules | state: :player1_turn, player1: :islands_set, player2: :islands_set}
   end
 
-  describe "rules" do
-    setup _ready_player1 do
-      rules = fixture(:ready_player1)
-      {:ok, [rules: rules]}
-    end
+  def fixture(:ready_player2) do
+    rules = Rules.new()
+    %Rules{rules | state: :player2_turn, player1: :islands_set, player2: :islands_set}
+  end
 
+  setup do
+    {:ok, [
+        ready_player1: fixture(:ready_player1),
+        ready_player2: fixture(:ready_player2)
+      ]}
+  end
+
+  describe "rules games setup" do
     test "starts initialised" do
       rules = Rules.new()
       assert rules.state == :initialised
@@ -90,10 +92,49 @@ defmodule IslandsEngine.RulesTest do
 
       assert rules.state == :player1_turn
     end
+  end
 
-    test "player 1 makes turn", ready_player1 do
-      rules = ready_player1.rules
-      IO.puts inspect(rules)
+  describe "rules game play" do
+    test "player 1 makes turn", %{ready_player1: rules} do
+      {:ok, rules} = Rules.check(rules, {:guess_coordinate, :player1})
+      assert rules.state == :player2_turn
+    end
+
+    test "player 2 cannot take turn during player 1s turn", %{ready_player1: rules} do
+      :error = Rules.check(rules, {:guess_coordinate, :player2})
+      assert rules.state == :player1_turn
+    end
+
+    test "player 2 makes turn", %{ready_player2: rules} do
+      {:ok, rules} = Rules.check(rules, {:guess_coordinate, :player2})
+      assert rules.state == :player1_turn
+    end
+
+    test "player 1 cannot take turn during player 2s turn", %{ready_player2: rules} do
+      :error = Rules.check(rules, {:guess_coordinate, :player1})
+      assert rules.state == :player2_turn
+    end
+  end
+
+  describe "rules game ends" do
+    test "player 1 does not win", %{ready_player1: rules} do
+      {:ok, rules} = Rules.check(rules, {:win_check, :no_win})
+      assert rules.state == :player1_turn
+    end
+
+    test "player 1 wins", %{ready_player1: rules} do
+      {:ok, rules} = Rules.check(rules, {:win_check, :win})
+      assert rules.state == :game_over
+    end
+
+    test "player 2 does not win", %{ready_player2: rules} do
+      {:ok, rules} = Rules.check(rules, {:win_check, :no_win})
+      assert rules.state == :player2_turn
+    end
+
+    test "player 2 wins", %{ready_player2: rules} do
+      {:ok, rules} = Rules.check(rules, {:win_check, :win})
+      assert rules.state == :game_over
     end
   end
 end
