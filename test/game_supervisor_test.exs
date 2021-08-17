fffbae377dad220470b8cbc23342c71a7299003e
Eskil Heyn Olsen <eskil@eskil.org>
2021-08-17 10:41:17 -0700

    Test cases for GameSupervisor

---
+++ test/game_supervisor_test.exs
@@ -0,0 +1,20 @@
+defmodule IslandsEngine.GameSupervisorTest do
+  use ExUnit.Case, async: true
+  alias IslandsEngine.GameSupervisor
+  doctest IslandsEngine.GameSupervisor, import: true
+
+  describe "game supervisor" do
+    test "init" do
+      {:ok, %{strategy: :simple_one_for_one}} = GameSupervisor.init(:ok)
+    end
+
+    test "start/stop game" do
+      {:error, {{:already_started, pid}, _}} = start_supervised({GameSupervisor, :ok})
+      IO.puts "\n#{inspect(pid)}"
+      {:ok, game} = GameSupervisor.start_game("king kong")
+      assert Process.alive?(game)
+      :ok = GameSupervisor.stop_game("king kong")
+      assert not Process.alive?(game)
+    end
+  end
+end
